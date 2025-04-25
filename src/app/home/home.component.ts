f27c93812164da67f1475b890ae871f185818acb
vivek1384 <160699490+vivek1384@users.noreply.github.com>
2025-04-25 10:30:36 +0530

    Changes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,17 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-import { HeaderComponent } from "../header/header.component";
-import { FooterComponent } from "../footer/footer.component";
+import { HeaderComponent } from '../header/header.component';
+import { FooterComponent } from '../footer/footer.component';
 import { RouterLink } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
   imports: [HeaderComponent, FooterComponent, RouterLink],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent implements OnInit {
-
   ngOnInit(): void {
-      window.scroll(0, 0)
+    window.scroll(0, 0);
   }
 }
