ec76ef3dfae4f51b60e52ad1b250a99904ca2dba
vivek1384 <160699490+vivek1384@users.noreply.github.com>
2025-04-26 09:38:17 +0530

    Update app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,7 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideAnimations } from '@angular/platform-browser/animations';
+
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
