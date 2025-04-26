b78eff52bb0c6c2ee874c160cac94d10f75f8c7e
vivek1384 <160699490+vivek1384@users.noreply.github.com>
2025-04-26 09:22:44 +0530

    Update header.component.ts

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,22 +2,33 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons';
+import {
+  trigger,
+  state,
+  style,
+  transition,
+  animate,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-header',
   imports: [RouterLink, RouterLinkActive, FontAwesomeModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
+  animations: [
+    trigger('slideInOut', [
+      state('hidden', style({ height: '0px', opacity: 0, overflow: 'hidden' })),
+      state('visible', style({ height: '*', opacity: 1 })),
+      transition('hidden <=> visible', animate('300ms ease-in-out')),
+    ]),
+  ],
 })
 export class HeaderComponent {
   faMenu = faBars;
   faClose = faXmark;
 
-  isMenu = false
-  openMenu(){
-    this.isMenu = true
-  }
-  closeMenu(){
-    this.isMenu = false;
+  isMenu = 'hidden';
+  openMenu() {
+    this.isMenu = this.isMenu === 'visible' ? 'hidden' : 'visible';
   }
 }
